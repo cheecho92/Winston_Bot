e70e3f615e10f755a85ad088e9bf47bc4938d5e0
cheecho92 <joshua.j.wisniewski@gmail.com>
2026-04-09 01:54:47 -0400

    added a message import from a user JSON file. Should all users to make their own messages, and the bot will import it.

--- handler_functions.py
+++ handler_functions.py
@@ -1,39 +1,39 @@
-from twitch_calls import chat_post
-from spotify_calls import check_queue, add_to_queue, get_song_info
+from winston_shared.twitch_calls import chat_post
+from winston_shared.spotify_calls import check_queue, add_to_queue, get_song_info
 
 # Handlers for the bots command list in the websocket_monitor.py file
-def handle_lurk(poster, twitch_config):
-    chat_post(twitch_config, f"@{poster}... Fine. I didn't want you here anyway... BigSad")
+def handle_lurk(poster, twitch_config, message):
+    chat_post(twitch_config, message['commands']['lurk'].format(poster=poster))
 
 
-def handle_ket(twitch_config):
-    chat_post(twitch_config, "meow")
+def handle_ket(twitch_config, message):
+    chat_post(twitch_config, message['commands']['ket'])
 
 
-def handle_kappa(poster, twitch_config):
+def handle_kappa(poster, twitch_config, message):
     if poster != "winstonkittybot":
-        chat_post(twitch_config, "Kappa")
+        chat_post(twitch_config, message['commands']['kappa'])
 
-def handle_praise(poster, text, twitch_config):
+def handle_praise(poster, text, twitch_config, message):
     if text == "good boy":
-        chat_post(twitch_config, f"@{poster} thank meow veowy mooch :3")
+        chat_post(twitch_config, message['commands']['praise'].format(poster=poster))
 
 
-def handle_song(poster, text, spotify_config, twitch_config):
-    check_queue(poster, text, spotify_config, twitch_config)
+def handle_song(poster, text, spotify_config, twitch_config, message):
+    check_queue(poster, text, spotify_config, twitch_config, message)
 
 
-def handle_next(poster, text, spotify_config, twitch_config):
-    check_queue(poster, text, spotify_config, twitch_config)
+def handle_next(poster, text, spotify_config, twitch_config, message):
+    check_queue(poster, text, spotify_config, twitch_config, message)
 
 
-def handle_discord(twitch_config):
-    chat_post(twitch_config, f"Join the stream discord: {twitch_config.discord} ")
+def handle_discord(twitch_config, message):
+    chat_post(twitch_config, message['commands']['discord'].format(discord_link=twitch_config.discord))
 
 
-def handle_request(text, spotify_config, poster, twitch_config):
+def handle_request(text, spotify_config, poster, twitch_config, message):
     try:
         song_uri, song_name, artist_name = get_song_info(text[35:], spotify_config, poster, twitch_config)
-        add_to_queue(song_uri, song_name, artist_name, spotify_config, poster, twitch_config)
+        add_to_queue(song_uri, song_name, artist_name, spotify_config, poster, twitch_config, message)
     except ValueError:
         raise
--- websocket_monitor.py
+++ websocket_monitor.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3.13
 import websockets
-from twitch_calls import eventsub_handler, parse_payload, chat_post, ban_user
+import os
+import json
+from winston_shared.twitch_calls import eventsub_handler, parse_payload, chat_post, ban_user
 from handler_functions import handle_praise, handle_discord, handle_kappa, handle_ket, handle_lurk, handle_next, handle_request, handle_song
 from configs import twitch, spotify
 
 
-# command dictionary. Passes poster and text through lambda to call them in the event loop
+# command dictionary. Passes poster, text, and custom messages through lambda to call them in the event loop
 command_dict = {
-    "!lurk": lambda poster, text: handle_lurk(poster, twitch),
-    "ket": lambda poster, text: handle_ket(twitch),
-    "kappa": lambda poster, text: handle_kappa(poster, twitch),
-    "!song": lambda poster, text: handle_song(poster, text, spotify, twitch),
-    "!next": lambda poster, text: handle_next(poster, text, spotify, twitch),
-    "!discord": lambda poster, text: handle_discord(twitch),
-    "!sr": lambda poster, text: handle_request(text, spotify, poster, twitch),
-    "good": lambda poster, text: handle_praise(poster, text, twitch),
+    "!lurk": lambda poster, text, message: handle_lurk(poster, twitch, message),
+    "ket": lambda poster, text, message: handle_ket(twitch, message),
+    "kappa": lambda poster, text, message: handle_kappa(poster, twitch, message),
+    "!song": lambda poster, text, message: handle_song(poster, text, spotify, twitch, message),
+    "!next": lambda poster, text, message: handle_next(poster, text, spotify, twitch, message),
+    "!discord": lambda poster, text, message: handle_discord(twitch, message),
+    "!sr": lambda poster, text, message: handle_request(text, spotify, poster, twitch, message),
+    "good": lambda poster, text, message: handle_praise(poster, text, twitch, message),
 }
 
 # async function to loop through twitch events
@@ -24,6 +26,9 @@ async def listen_twitch():
 
         with open('banned_phrases.txt') as f:
             banned_phrases = [line.rstrip() for line in f]
+
+        with open(os.getenv("MESSAGE_FILE")) as f:
+            message = json.load(f)
 
         # Listen to twitch websocket server
         async for event in ws:
@@ -38,7 +43,7 @@ async def listen_twitch():
                 continue
 
             if event_type == "channel.follow":
-                chat_post(twitch, f"@{follower}, thanks for the follow!")
+                chat_post(twitch, message['commands']['follow'].format(follower=follower))
 
             elif event_type == "channel.suspicious_user.message":
                 ban_user(poster, twitch)
@@ -55,6 +60,6 @@ async def listen_twitch():
                 # call the command list based on the first word in 'text.' Edge case is needed for !sr, as it must intake user text.
                 elif command in command_dict:
                     try:
-                        command_dict[command](poster, text)
+                        command_dict[command](poster, text, message)
                     except ValueError:
                         continue
